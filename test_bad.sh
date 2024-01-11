9116771e3a839667532d213fb5ca19f9a8f9d024
Tomasz Kubica <tk429368@students.mimuw.edu.pl>
2024-01-11 23:08:31 +0100

    Add testing scripts

---
+++ test_bad.sh
@@ -0,0 +1,4 @@
+for file in official_examples/bad/*.lat
+do
+  ./LatteTypeChecker "$file"
+done
---
+++ test_output.sh
@@ -0,0 +1,26 @@
+
+# Compile compiler
+make
+
+# Compile examples using compiler
+./LatteCompiler official_examples/good/*.lat
+
+# Run compiled examples and compare output with expected output
+for file in official_examples/good/*.lat
+do
+  # If there is input file for program, then pass it to program
+  if [ -f "${file%.*}.input" ]
+  then
+    lli ${file%.*}.bc < "${file%.*}.input" > "${file%.*}.result"
+  else
+    lli ${file%.*}.bc > "${file%.*}.result"
+  fi
+  diff "${file%.*}.result" "${file%.*}.output"
+  # If diff returns 0, then files are the same
+  if [ $? -eq 0 ]
+  then
+    echo "Test ${file%.*} passed"
+  else
+    echo "Test ${file%.*} failed"
+  fi
+done
